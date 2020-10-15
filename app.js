9a17809da154282559963e34f7af3527439d549e
Mitchell Gams <mitchgams@gmail.com>
2020-10-15 15:44:22 -0500

    started to throw some things in an object, but stopped because it began to get messy

--- app.js
+++ app.js
@@ -5,47 +5,48 @@ document.body.appendChild(button);
 
 document.getElementsByTagName("button")[0].addEventListener("click", addSquare);
 
-let squareNum = 0;
-let colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-                '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-                '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-                '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-                '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-                '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-                '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-                '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-                '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-                '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+
+let sq = {
+    colors: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
+    num: 0,
+    listener: function() {
+        for(const square of document.getElementsByClassName("item")) {
+            /* found a way to add parameters to functions within addEventListener by using .bind */
+            square.addEventListener("mouseover", idShown.bind(this, true));
+            square.addEventListener("mouseout", idShown.bind(this, false));
+            square.addEventListener("click", changeColor);
+            square.addEventListener("dblclick", doubleClick);
+        }
+    },
+}
 
 
 function addSquare(e) {
-//    console.log("ok");
     let newDiv = document.createElement("DIV");
     newDiv.classList.add("item");
-    newDiv.id = squareNum;
+    newDiv.id = sq.num;
     let container = document.getElementsByClassName("container");
     container[0].appendChild(newDiv);
-    squareNum++;
-    runListener();
+    sq.num++;
+    sq.listener();
 }
-let squares = document.getElementsByClassName("item");
-function runListener() {
-    for(const square of squares) {
-        /* found a way to add parameters to functions within addEventListener by using .bind */
-        square.addEventListener("mouseover", idShown.bind(this, true));
-        square.addEventListener("mouseout", idShown.bind(this, false));
-        square.addEventListener("click", changeColor);
-        square.addEventListener("dblclick", doubleClick);
-    }
-    //console.log(squares);
-}
+
 function idShown(show, e) {
     let fetchSquare = document.getElementById(e.target.id);
     let squareText = null;
     show ? (fetchSquare.textContent = e.target.id) : (fetchSquare.textContent = "");
 }
 function changeColor(e) {
-    document.getElementById(e.target.id).style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    document.getElementById(e.target.id).style.backgroundColor = sq.colors[Math.floor(Math.random() * sq.colors.length)];
 }
 function doubleClick(e) {
     let searchNum = 1;
@@ -68,7 +69,7 @@ function doubleClick(e) {
         while(squareAfter === null) {
             searchNum++;
             squareAfter = document.getElementById(id+searchNum);
-            if(id+searchNum > squareNum) { 
+            if(id+searchNum > sq.num) { 
                 alert("no square after this.")
                 break;
             }
