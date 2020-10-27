468f15618c6265588fb2680e2d2e9b7520cbf114
Mitchell Gams <mitchgams@gmail.com>
2020-10-27 13:48:41 -0500

    simplified some js and edited style

--- app.js
+++ app.js
@@ -7,40 +7,28 @@ document.getElementsByTagName("button")[0].addEventListener("click", addSquare);
 
 
 let sq = {
-    colors: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
+    colors: ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933', '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
     num: 0
 }
-
-
 let container = document.getElementsByClassName("container");
+
 function addSquare(e) {
     let square = document.createElement("DIV");
     square.classList.add("item");
     square.id = sq.num;
-    container[0].appendChild(square);
     square.addEventListener("mouseover", idShown.bind(this, true));
     square.addEventListener("mouseout", idShown.bind(this, false));
     square.addEventListener("click", changeColor);
     square.addEventListener("dblclick", doubleClick);
+    container[0].appendChild(square);
     sq.num++;
 }
 
 function idShown(show, e) {
-    let fetchSquare = document.getElementById(e.target.id);
-    let squareText = null;
-    show ? (fetchSquare.textContent = e.target.id) : (fetchSquare.textContent = "");
+    show ? (e.target.textContent = e.target.id) : (e.target.textContent = "");
 }
 function changeColor(e) {
-    document.getElementById(e.target.id).style.backgroundColor = sq.colors[Math.floor(Math.random() * sq.colors.length)];
+    e.target.style.backgroundColor = sq.colors[Math.floor(Math.random() * sq.colors.length)];
 }
 function doubleClick(e) {
     //let searchNum = 1;
